27a1aa4601d1ac4684f26a450595d4ddef6bc070
mostafaElspagh0 <mostafaelspagh0@gmail.com>
2021-12-14 05:45:57 +0200

    setup local db with default Config

---
+++ backend/services/database/db.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const config = require('config');
+
+function dbURI() {
+    const dbConfig = config.get('database_config');
+    if (dbConfig.type !== 'mongodb') {
+        throw new Error('Invalid database type');
+    }
+    if (!dbConfig.host || !dbConfig.port) {
+        throw new Error('Invalid database configuration');
+    }
+    if (dbConfig.username && dbConfig.password) {
+        return `mongodb://${dbConfig.username}:${dbConfig.password}@${dbConfig.host}:${dbConfig.port}/${dbConfig.name}`;
+    }
+    return `mongodb://${dbConfig.host}:${dbConfig.port}/${dbConfig.name}`;
+}
+
+function registerModels() {
+}
+
+function registerEventHandlers() {
+    mongoose.connection.on('connected', function () {
+        console.log('Mongoose default connection open to ' + dbURI());
+    });
+
+    mongoose.connection.on('error', function (err) {
+        console.log('Mongoose default connection error: ' + err);
+    });
+
+    mongoose.connection.on('disconnected', function () {
+        console.log('Mongoose default connection disconnected');
+    });
+
+    process.on('SIGINT', function () {
+        mongoose.connection.close(function () {
+            console.log('Mongoose default connection disconnected through app termination');
+            process.exit(0);
+        });
+    });
+}
+
+async function initDb() {
+    await mongoose.connect(dbURI());
+    registerEventHandlers();
+    registerModels();
+}
+
+module.exports = {
+    initDb
+}
