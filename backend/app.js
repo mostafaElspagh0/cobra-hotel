d51d56c38807909051e8da666d19d00749ae73e0
mostafaElspagh0 <mostafaelspagh0@gmail.com>
2021-12-14 03:32:40 +0200

    add live endpoint

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,11 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 
+const liveRouter = require('./routes/live');
+
 const app = express();
+
+app.use('/live', liveRouter);
 
 app.use(logger('dev'));
 app.use(express.json());
