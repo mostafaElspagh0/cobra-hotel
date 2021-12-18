578c8b1fbd356e5d3205762020a6964baffb9706
mostafaElspagh0 <mostafaelspagh0@gmail.com>
2021-12-18 05:57:05 +0200

    move middle layer before routing

--- backend/app.js
+++ backend/app.js
@@ -1,28 +1,24 @@
 const express = require('express');
-const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const config = require('config')
 const logger = require('morgan');
 const liveRouter = require('./routes/live');
 const authRouter = require('./routes/auth');
-
 const bodyParser = require("body-parser");
 const app = express();
+
+app.use(logger('dev'));
+app.use(cors({
+    origin: config.get('corsOrigin'),
+    credentials: true
+}));
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
 
 app.use('/', liveRouter);
 app.use('/live', liveRouter);
 app.use('/auth',authRouter);
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cors({
-    origin: config.get('corsOrigin'),
-    credentials: true
-}));
-app.use(cookieParser());
 
-app.use(bodyParser.urlencoded({extended : true}));
-app.use(bodyParser.json());
 
 module.exports = app;
