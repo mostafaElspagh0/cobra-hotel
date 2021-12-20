eaf3d0db6e0e0478fcdb6bb8a4206f5170e295e8
AbdelrahmanMohammedSayed <91883530+AbdelrahmanMohammedSayed@users.noreply.github.com>
2021-12-20 21:16:40 +0200

    add async for register

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -9,7 +9,6 @@ const express = require("express"),
 
      {check, validationResult} = require("express-validator");
 
-//-------------------
 router.post('/register',
     [
         check('name', 'Name is required') .isLength({min:3,max:25}).notEmpty(),
@@ -18,8 +17,41 @@ router.post('/register',
         check('password','Please enter a password with 6 or more characters').exists().isLength({ min: 5 }),
         check('job_type', 'job type is required to be null').isIn(['Manager', 'Hr','Receptionist', 'Barista']),
         check('phone').isLength({min:11,max:11}).isNumeric,
-    ],);
-//------------------------------------------
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        let { name,userNme,job_type, email, password,phone, address } = req.body;
+        const user = new user({
+            name,
+            userNme,
+            job_type,
+            email,
+            password,
+            phone,
+            address
+
+        });
+        try {
+            let userExist = await User.findOne({ email });
+            if (userExist) {
+                return res
+                    .status(400)
+                    .json({ errors: [{ msg: 'User already exists' }] });
+            }
+            await user.save();
+            const userObj=user.toObject();
+            delete userObj.password;
+            res.json({msg:'User created',user : userObj });
+        }
+        catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server Error');
+        }
+    }
+);
 
 
 
