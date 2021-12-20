b37c73f85629e57988d6b49ae1685c629d49c5d5
mostafaElspagh0 <mostafaelspagh0@gmail.com>
2021-12-20 02:31:53 +0200

    delete bad code

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -8,40 +8,6 @@ const express = require("express"),
     User = require("./services/database/models/user"),
 
      {check, validationResult} = require("express-validator");
-      mongoose.connect('../services/database/models/user.js');
-router.post('/register',
-    [
-        check('name', 'Name is required').isLength({min :3, max: 25}).notEmpty(),
-        check('email', 'Please include a valid email').isEmail(),
-        check('password','Please enter a password with 6 or more characters').isLength({ min: 6 }),
-        check('userName', 'userName is required ').isEmpty(),
-        check('job_type','job_type is required').notEmpty,
-        check('phone').isLength({min: 11, max: 11}).isNumeric()
-    ],
-    async (req, res) => {
-        let errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
-        let { name, email, password, job_type,phone, address } = req.body;
-        const user = new User({name, email, password, job_type,phone, address});
-        try {
-            let user = await User.findOne({ email });
-            if (user) {
-                return res.status(400).json({ errors: [{ msg: 'User already exists' }] });
-            }
-            await user.save();
-            const userObj =user.toObject();
-            delete userObj.password;
-            res.json({msg: 'user created'user :userObj});
-            }
-
-        catch (err) {
-            console.error(err.message);
-            res.status(500).send('Server Error');
-        }
-    }
-);
 
 
 router.post('/login',
@@ -87,16 +53,5 @@ router.post('/login',
         }
     }
 );
-router.get('/getAllUsers',(req, res, next)=>{
-    User.find({},(err,resualt)=>{
-        if(err){
-            console.log(err)
-            res.redirect('/');
-        }else {
-            console.log(resualt)
-            res.render('index',{items : resualt})
-        }
 
-    })
-})
 module.exports = router;
