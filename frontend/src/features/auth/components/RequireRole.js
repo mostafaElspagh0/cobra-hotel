a93752a5fa66fbf383303ca9d20731cd84982f3a
mostafaElspagh0 <mostafaelspagh0@gmail.com>
2021-12-21 11:39:38 +0200

    add require Role Component

---
+++ frontend/src/features/auth/components/RequireRole.js
@@ -0,0 +1,21 @@
+import {Fragment, useContext} from 'react';
+import {AuthContext} from '../AuthContext';
+import {Navigate} from "react-router-dom";
+
+const RequireRole = ({roles, children}) => {
+    const {getRole, isAuthenticated} = useContext(AuthContext);
+    if(!isAuthenticated) {
+        return <Navigate to="/login"/>
+    }
+    if (roles.includes(getRole())) {
+        return children;
+    }
+    // TODO: style this page
+    return (
+        <Fragment>
+            you are not authorized to view this page
+        </Fragment>
+    );
+};
+
+export default RequireRole;
