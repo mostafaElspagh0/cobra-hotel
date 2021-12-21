a912936e4bd0b7a8545b7d05e4386f3a1aaab8de
mostafaElspagh0 <mostafaelspagh0@gmail.com>
2021-12-21 08:00:55 +0200

    make index redirect to dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, {Fragment} from 'react';
 import SignInPage from "./features/auth/components/SignInPage";
-import {Route, Routes} from "react-router-dom";
+import {Navigate, Route, Routes} from "react-router-dom";
 import DashBoardPage from "./features/dashBoard/components/DashBoardPage";
 function App() {
     return (
@@ -8,7 +8,7 @@ function App() {
             <Routes>
                 <Route path="/login" element={<SignInPage />} />
                 <Route path="/dashboard/*" element={<DashBoardPage/>} />
-                <Route path="/" element={<DashBoardPage/>} />
+                <Route path="/" element={<Navigate path='/' to='dashboard'/>} />
 
             </Routes>
         </Fragment>
