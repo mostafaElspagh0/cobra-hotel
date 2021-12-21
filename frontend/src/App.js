93afc1cb70003b6cc4c4bac878e9fec3d6e65897
mostafaElspagh0 <mostafaelspagh0@gmail.com>
2021-12-21 12:02:18 +0200

    add employee tap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,9 @@
 import React, {Fragment} from 'react';
 import SignInPage from "./features/auth/components/SignInPage";
 import {Navigate, Route, Routes, useParams} from "react-router-dom";
+import RequireRole from "./features/auth/components/RequireRole";
 import DashBoardPage from "./features/dashBoard/components/DashBoardPage";
+import Hr from "./features/Hr/Hr";
 const Com = () => {
         let c = useParams();
         return <div>{c.id}</div>
@@ -13,6 +15,8 @@ function App() {
             <Routes>
                 <Route path="/login" element={<SignInPage />} />
                 <Route path="/dashboard" element={<DashBoardPage/>} >
+                    <Route path="employee" element={<RequireRole roles={['Manager','Hr']}><Hr/></RequireRole>} />
+                    <Route path="Orders" element={<RequireRole roles={['Manager']}><Com/>sadfasflkj</RequireRole>} />
                     <Route path=":id" element={<Com/>} />
                 </Route>
                 <Route path="/" element={<Navigate path='/' to='dashboard'/>} />
