8664ac7f9bc45ff5346a73b176870b0637c5424a
mostafaElspagh0 <mostafaelspagh0@gmail.com>
2021-12-21 10:19:32 +0200

    use nested routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,20 @@
 import React, {Fragment} from 'react';
 import SignInPage from "./features/auth/components/SignInPage";
-import {Navigate, Route, Routes} from "react-router-dom";
+import {Navigate, Route, Routes, useParams} from "react-router-dom";
 import DashBoardPage from "./features/dashBoard/components/DashBoardPage";
+const Com = () => {
+        let c = useParams();
+        return <div>{c.id}</div>
+
+};
 function App() {
     return (
         <Fragment>
             <Routes>
                 <Route path="/login" element={<SignInPage />} />
-                <Route path="/dashboard/*" element={<DashBoardPage/>} />
+                <Route path="/dashboard" element={<DashBoardPage/>} >
+                    <Route path=":id" element={<Com/>} />
+                </Route>
                 <Route path="/" element={<Navigate path='/' to='dashboard'/>} />
 
             </Routes>
