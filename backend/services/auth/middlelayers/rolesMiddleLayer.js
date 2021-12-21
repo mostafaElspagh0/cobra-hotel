d5ba4dc64c0d257cbf1596e0287ac68dc2ad307b
mostafaElspagh0 <mostafaelspagh0@gmail.com>
2021-12-21 15:25:42 +0200

    fix rbac middle layer

--- backend/services/auth/middlelayers/rolesMiddleLayer.js
+++ backend/services/auth/middlelayers/rolesMiddleLayer.js
@@ -1,24 +1,24 @@
 const validateToken = require('../utils/validateToken');
-const isA = (job_types) =>{
-   return async (req,res,next)=>{
-       const token = req.header('x-auth-token');
-       if (!token){
-           return res.status(404).json({errors:[ { msg: 'NO Token ,authorization denied '}]});
-       }
-       try {
-           const decoded = await  validateToken(token);
-           if (!decoded){
-               return res.status(401).json({errors:[{msg:'Token is not '}]})
-           }
-           if(!(decoded.user.job_type in job_types)){
-               return res.status(401).json({errors:[{msg:'you ara not authorization'}]})
-           }
-           req.user = decoded.user;
-           next();
-       }catch (err){
-           res.status(401).json({errors:[{msg:'Token is not ' }]});
-       }
-   };
+const isA = (job_types) => {
+    return async (req, res, next) => {
+        const token = req.header('x-auth-token');
+        if (!token) {
+            return res.status(404).json({errors: [{msg: 'NO Token ,authorization denied '}]});
+        }
+        try {
+            const decoded = await validateToken(token);
+            if (!decoded) {
+                return res.status(401).json({errors: [{msg: 'Token is not '}]})
+            }
+            if (!job_types.includes(decoded.user.job_type)) {
+                return res.status(401).json({errors: [{msg: 'you ara not authorization'}]})
+            }
+            req.user = decoded.user;
+            next();
+        } catch (err) {
+            res.status(401).json({errors: [{msg: 'Token is not df'}]});
+        }
+    };
 
 };
 
