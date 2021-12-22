36e3382304391b504b0f31cf308940216941c296
jonathan1548 <jonathan.ashraf@yahoo.com>
2021-12-22 22:02:25 +0200

    add announcement model

---
+++ backend/services/database/models/announcement.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const announcementScheme = new Schema(
+    {
+        issued_by : {
+            _id: String ,
+            required : true
+        },
+        target_audience : {
+            type : String ,
+            required:true,
+            enum: ['Manager', 'Hr','Receptionist', 'Barista'],
+            default: 'All',
+        },
+        title :{
+            type :   String
+        },
+        body :{
+          type :   String
+        },
+        created_at: {
+            type: Date,
+            default: Date.now
+        },
+        updated_at: {
+            type: Date,
+            default: Date.now
+        }
+    }
+)
+const announcementModel = mongoose.model('announcement', announcementScheme);
+module.exports = announcementModel;
