69d77cc35da919b64aac1668873c763873004b79
mostafaElspagh0 <mostafaelspagh0@gmail.com>
2021-12-23 04:40:46 +0200

    wire up hr Page

--- frontend/src/features/hr/components/Hr.js
+++ frontend/src/features/hr/components/Hr.js
@@ -1,64 +1,144 @@
 import * as React from 'react';
 import {Container} from "@mui/material";
-import {Fragment} from "react";
+import {Fragment, useContext, useEffect, useState} from "react";
 import {DataGrid} from '@mui/x-data-grid';
 import Grid from "@mui/material/Grid";
-import {FormControl, InputAdornment, OutlinedInput} from "@material-ui/core";
+import {
+    FormControl,
+    InputAdornment,
+    OutlinedInput, Paper,
+} from "@material-ui/core";
 import SearchIcon from '@mui/icons-material/Search';
+import IconButton from "@mui/material/IconButton";
+import EditIcon from '@mui/icons-material/Edit';
+import * as Api from "../api/employeeApi"
+import {AuthContext} from "../../auth/context/AuthContext";
+import {Outlet, useLocation, useNavigate} from "react-router-dom";
 
-const columns = [
-    {field: 'id', headerName: 'ID', width: 70, editable: true},
-    {field: 'firstName', headerName: 'First name', width: 130},
-    {field: 'lastName', headerName: 'Last name', width: 130},
-    {
-        field: 'age',
-        headerName: 'Age',
-        type: 'number',
-        width: 90,
-    },
-    {
-        field: 'fullName',
-        headerName: 'Full name',
-        description: 'This column has a value getter and is not sortable.',
-        sortable: false,
-        width: 160,
-        valueGetter: (params) =>
-            `${params.getValue(params.id, 'firstName') || ''} ${
-                params.getValue(params.id, 'lastName') || ''
-            }`,
-    },
-];
-
-const rows = [
-    {id: 1, lastName: 'Snow', firstName: 'Jon', age: 35},
-    {id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42},
-    {id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45},
-    {id: 4, lastName: 'Stark', firstName: 'Arya', age: 16},
-    {id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null},
-    {id: 6, lastName: 'Melisandre', firstName: null, age: 150},
-    {id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44},
-    {id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36},
-    {id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65},
-    {id: 10, lastName: "fdsjif" , firstName: "fjd",age: 42},
-    {id: 11, lastName: 'Snow', firstName: 'Jon', age: 35},
-    {id: 12, lastName: 'Lannister', firstName: 'Cersei', age: 42},
-    {id: 13, lastName: 'Lannister', firstName: 'Jaime', age: 45},
-    {id: 14, lastName: 'Stark', firstName: 'Arya', age: 16},
-    {id: 15, lastName: 'Targaryen', firstName: 'Daenerys', age: null},
-    {id: 16, lastName: 'Melisandre', firstName: null, age: 150},
-    {id: 17, lastName: 'Clifford', firstName: 'Ferrara', age: 44},
-    {id: 18, lastName: 'Frances', firstName: 'Rossini', age: 36},
-    {id: 19, lastName: 'Roxie', firstName: 'Harvey', age: 65},
-    {id: 20, lastName: "fdsjif" , firstName: "fjd",age: 42},
-
-
-];
 
 const Hr = () => {
+    const {getToken} = useContext(AuthContext);
+    const [rows, setRows] = useState({users: [], count: 0});
+    const [isLoading, setIsLoading] = useState(true);
+    const [page, setPage] = useState(0);
+    const Location = useLocation();
+    const Navigate = useNavigate();
+    const columns = [
+        {
+            field: '_id',
+            headerName: 'Edit',
+            renderCell: ({id}) => {
+                return (
+                    <IconButton onClick={
+                        (e) => Navigate(`edit/${id}`)
+                    }>
+                        <EditIcon/>
+                    </IconButton>
+                )
+            },
+            sortable: false,
+            filterable: false,
+        },
+        {
+            field: 'name',
+            headerName: 'Name',
+            sortable: false,
+            filterable: false
+        },
+        {
+            field: 'job_type',
+            headerName: 'job',
+            sortable: false,
+            filterable: false
+        },
+        {
+            field: 'email', headerName: 'email',
+            width: 250,
+            sortable: false,
+            filterable: false
+        },
+        {
+            field: 'password',
+            headerName: 'password',
+            hide: true,
+        },
+        {
+            field: 'created_at',
+            headerName: 'Year',
+            type: 'dateTime',
+            hide: true,
+            valueGetter: ({value}) => value && new Date(value),
+        },
+        {
+            field: 'updated_at',
+            headerName: 'updated_at',
+            type: 'dateTime',
+            hide: true,
+            valueGetter: ({value}) => value && new Date(value),
+        },
+        {
+            field: '__v',
+            headerName: 'Name',
+            hide: true
+        }
+    ]
+    const a = 1;
+    useEffect(() => {
+            setIsLoading(true);
+            const fetchData = async () => {
+                Api.getEmployees(getToken()).then(
+                    (res) => {
+                        setRows(res.data)
+                        setIsLoading(false);
+                    }
+                )
+            }
+            fetchData();
+        }, [a]
+    )
 
+    const handlePageChange = (page) => {
+        setIsLoading(true);
+        Api.getEmployees(getToken(), page).then(
+            (res) => {
+                setRows(res.data)
+                setPage(page);
+                setIsLoading(false);
+            }
+        )
+    }
+
+    const isPopup = () => {
+        return Location.pathname.includes('/edit')
+    }
     return (
         <Fragment>
-            <Container>
+            {isPopup() &&
+
+                <Paper
+                    style={{
+                        position: 'absolute',
+                        top: '50%',
+                        left: '50%',
+                        transform: 'translate(-50%, -50%)',
+                        width: '80%',
+                        height: '80%',
+                        padding: '20px',
+                        zIndex: '1000'
+                    }}>
+                    <Outlet/>
+                </Paper>
+            }
+            <Container style={
+                (isPopup()) ? {
+                    WebkitFilter: 'blur(5px)',
+                    MozFilter: 'blur(5px)',
+                    MsFilter: 'blur(5px)',
+                    OFilter: 'blur(5px)',
+                    filter: 'blur(5px)',
+                    pointerEvents: 'none'
+                } : {}
+            }>
                 <Grid container spacing={3} justifyContent={'center'}>
                     <Grid item xs={12}/>
                     <Grid item xs={12} sm={6}>
@@ -74,9 +154,19 @@ const Hr = () => {
                     </Grid>
                     <Grid item xs={12}>
                         <DataGrid
-                            rows={rows}
-                            columns={columns}
+                            loading={isLoading}
+                            getRowId={(row) => row._id}
+                            rows={rows.users}
+                            disableSelectionOnClick
                             pageSize={10}
+                            page={page}
+                            onPageChange={(e, g,) => {
+                                handlePageChange(e)
+                            }}
+                            rowsPerPageOptions={[]}
+                            rowCount={rows.count}
+                            paginationMode={"server"}
+                            columns={columns}
                             style={
                                 {
                                     minHeight: "65vh",
