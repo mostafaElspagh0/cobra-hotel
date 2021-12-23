6b68ee32ae7fad96cc30c28b4ea8ad8d1c38772d
mostafaElspagh0 <mostafaelspagh0@gmail.com>
2021-12-23 04:42:22 +0200

    add route for edit employee

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import {Navigate, Route, Routes, useParams} from "react-router-dom";
 import RequireRole from "./features/auth/components/RequireRole";
 import DashBoardPage from "./features/dashBoard/components/DashBoardPage";
 import Hr from "./features/hr/components/Hr";
+import EditEmployee from "./features/hr/components/EditEmployee";
 const Com = () => {
         let c = useParams();
         return <div>{c.id}</div>
@@ -15,7 +16,9 @@ function App() {
             <Routes>
                 <Route path="/login" element={<SignInPage />} />
                 <Route path="/dashboard" element={<DashBoardPage/>} >
-                    <Route path="employee" element={<RequireRole roles={['Manager','Hr']}><Hr/></RequireRole>} />
+                    <Route path="employee" element={<RequireRole roles={['Manager','Hr']}><Hr/></RequireRole>} >
+                        <Route path="edit/:id" element={<EditEmployee/>} />
+                    </Route>
                     <Route path="Orders" element={<RequireRole roles={['Manager']}><Com/>sadfasflkj</RequireRole>} />
                     <Route path=":id" element={<Com/>} />
                 </Route>
