9dcd4fa8fa51639b9f026417a872d97bbfd76aea
mostafaElspagh0 <mostafaelspagh0@gmail.com>
2021-12-24 08:32:41 +0200

    add Announcement route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import EditEmployee from "./features/hr/components/EditEmployee";
 import ForgetPassword from "./features/auth/components/ForgetPassword";
 import Orders from "./features/Orders/Component/Orders";
 import SendEmailPage from "./features/mail/component/SendEmailPage";
+import Announcement from "./features/Announcment/Announcement";
 const Com = () => {
         let c = useParams();
         return <div>{c.id}</div>
@@ -25,7 +26,7 @@ function App() {
                     </Route>
                     <Route path="email" element={<RequireRole roles={['Manager','Hr']}><SendEmailPage/></RequireRole>} />
                     <Route path="Orders" element={<RequireRole roles={['Manager','Barista']}><Orders/></RequireRole>} />
-                    <Route path="Announcement" element={<RequireRole roles={['Manager',"Hr"]}><Com/>sadfasflkj</RequireRole>} />
+                    <Route path="Announcement" element={<RequireRole roles={['Manager',"Hr"]}><Announcement/></RequireRole>} />
                     <Route path=":id" element={<Com/>} />
                 </Route>
                 <Route path="/" element={<Navigate path='/' to='dashboard'/>} />
