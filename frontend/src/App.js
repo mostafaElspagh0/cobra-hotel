ab99e1d6fdd9ace4e75e907db869a850e0aa26e9
mostafaElspagh0 <mostafaelspagh0@gmail.com>
2021-12-24 07:35:02 +0200

    add forget password route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,34 +5,27 @@ import RequireRole from "./features/auth/components/RequireRole";
 import DashBoardPage from "./features/dashBoard/components/DashBoardPage";
 import Hr from "./features/hr/components/Hr";
 import EditEmployee from "./features/hr/components/EditEmployee";
-import SendEmailPage from "./features/mail/component/SendEmailPage";
-
+import ForgetPassword from "./features/auth/components/ForgetPassword";
 const Com = () => {
-    let c = useParams();
-    return <div>{c.id}</div>
+        let c = useParams();
+        return <div>{c.id}</div>
 
 };
-
 function App() {
     return (
         <Fragment>
             <Routes>
-                <Route path="/login" element={<SignInPage/>}/>
-                <Route path="/dashboard" element={<DashBoardPage/>}>
-                    <Route path="employee" element={<RequireRole roles={['Manager', 'Hr']}><Hr/></RequireRole>}>
-                        <Route path="edit/:id" element={<EditEmployee/>}/>
+                <Route path="/login" element={<SignInPage />} />
+                <Route path="/forgot-password" element={<ForgetPassword />} />
+                <Route path="/dashboard" element={<DashBoardPage/>} >
+                    <Route path="employee" element={<RequireRole roles={['Manager','Hr']}><Hr/></RequireRole>} >
+                        <Route path="edit/:id" element={<EditEmployee/>} />
                     </Route>
-                    <Route path="Orders" element={<RequireRole roles={['Manager']}><Com/>sadfasflkj</RequireRole>}/>
-                    <Route path="Announcement"
-                           element={<RequireRole roles={['Manager', "Hr"]}><Com/>sadfasflkj</RequireRole>}/>
-                    <Route path={"email"}
-                           element={
-                               <RequireRole roles={['Manager', "Hr"]}>
-                                   <SendEmailPage/>
-                               </RequireRole>}/>
-                    <Route path=":id" element={<Com/>}/>
+                    <Route path="Orders" element={<RequireRole roles={['Manager']}><Com/>sadfasflkj</RequireRole>} />
+                    <Route path="Announcement" element={<RequireRole roles={['Manager',"Hr"]}><Com/>sadfasflkj</RequireRole>} />
+                    <Route path=":id" element={<Com/>} />
                 </Route>
-                <Route path="/" element={<Navigate path='/' to='dashboard'/>}/>
+                <Route path="/" element={<Navigate path='/' to='dashboard'/>} />
 
             </Routes>
         </Fragment>
