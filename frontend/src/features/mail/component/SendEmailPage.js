2eb8f390bec0c0ae187a9fb23ed16832c1297bc9
mostafaElspagh0 <mostafaelspagh0@gmail.com>
2021-12-24 06:32:14 +0200

    add form validation to send email from

--- frontend/src/features/mail/component/SendEmailPage.js
+++ frontend/src/features/mail/component/SendEmailPage.js
@@ -5,43 +5,132 @@ import TextareaAutosize from "@mui/material/TextareaAutosize";
 import Button from "@mui/material/Button";
 import * as React from "react";
 import Container from "@mui/material/Container";
+import {Controller, useForm} from "react-hook-form";
+import Box from "@mui/material/Box";
 
 const SendEmailPage = () => {
+    const {handleSubmit, control} = useForm();
+    const onSubmit = data => {
+        console.log(data);
+    };
     return (
         <Fragment>
             <Container>
-                <Grid container spacing={5}>
-                    <Grid item xs={12}/>
-                    <Grid item xs={12} sm={6}>
-                        <TextField id="outlined-search" label="Title" type="search" fullWidth/>
+                <Box component={"form"} onSubmit={handleSubmit(onSubmit)} noValidate>
+                    <Grid container spacing={5}>
+                        <Grid item xs={12}/>
+                        <Grid item xs={12} sm={6}>
+                            <Controller
+                                name="title"
+                                control={control}
+                                defaultValue=""
+                                render={({field: {onChange, value}, fieldState: {error}}) => (
+                                    <TextField
+                                        label="to"
+                                        variant="outlined"
+                                        fullWidth
+                                        required
+                                        value={value}
+                                        onChange={onChange}
+                                        error={!!error}
+                                        helperText={error ? error.message : null}
+                                        type="text"
+                                    />
+                                )}
+                                rules={{
+                                    required: 'title is required',
+                                }}
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <Controller
+                                name="to"
+                                control={control}
+                                defaultValue=""
+                                render={({field: {onChange, value}, fieldState: {error}}) => (
+                                    <TextField
+                                        label="to"
+                                        variant="outlined"
+                                        fullWidth
+                                        required
+                                        value={value}
+                                        onChange={onChange}
+                                        error={!!error}
+                                        helperText={error ? error.message : null}
+                                        type="email"
+                                    />
+                                )}
+                                rules={{
+                                    required: 'Email required',
+                                    pattern: {
+                                        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                                        message: 'invalid email address'
+                                    }
+                                }}
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <Controller
+                                name="Subject"
+                                control={control}
+                                defaultValue=""
+                                render={({field: {onChange, value}, fieldState: {error}}) => (
+                                    <TextField
+                                        label="subject"
+                                        variant="outlined"
+                                        fullWidth
+                                        required
+                                        value={value}
+                                        onChange={onChange}
+                                        error={!!error}
+                                        helperText={error ? error.message : null}
+                                        type="text"
+                                    />
+                                )}
+                                rules={{
+                                    required: 'subject is required',
+                                }}
+                            />
+
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Controller
+                                name="body"
+                                control={control}
+                                defaultValue=""
+                                render={({field: {onChange, value}, fieldState: {error}}) => (
+                                    <TextareaAutosize
+                                        aria-label="minimum height"
+                                        minRows={8}
+                                        placeholder="Your E-mail"
+                                        resize="none"
+                                        style={{
+                                            width: "100%",
+                                            borderRadius: "5px",
+                                            padding: "10px",
+                                            resize: "none"
+                                        }}
+                                        label="subject"
+                                        required
+                                        value={value}
+                                        onChange={onChange}
+                                        error={!!error}
+                                        helperText={error ? error.message : null}
+                                        type="text"
+                                    />
+                                )}
+                                rules={{
+                                    required: 'subject is required',
+                                }}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Button variant="contained" type={"submit"}>
+                                Send E-mail
+                            </Button>
+                        </Grid>
                     </Grid>
-                    <Grid item xs={12} sm={6}>
-                        <TextField id="outlined-search" label="To" type="search" fullWidth/>
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                        <TextField id="outlined-search" label="Subject" type="search"
-                                   fullWidth/>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <TextareaAutosize
-                            aria-label="minimum height"
-                            minRows={ 8 }
-                            placeholder="Your E-mail"
-                            resize="none"
-                            style={{
-                                width: "100%",
-                                borderRadius: "5px",
-                                padding: "10px",
-                                resize: "none"
-                            }}
-                        />
-                    </Grid>
-                    <Grid item xs={12} >
-                        <Button variant="contained" >
-                            Send E-mail
-                        </Button>
-                    </Grid>
-                </Grid>
+                </Box>
             </Container>
         </Fragment>
 
