50e8d55fd162bdd0f97fdf7fd456a03644558b87
MoOsman5 <mohamed1592000gmail.com>
2021-12-24 16:03:01 +0200

    adding price to room scheme

--- backend/services/database/models/rooms.js
+++ backend/services/database/models/rooms.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const bcrypt = require('bcryptjs');
 const config = require('config');
+//
 const roomScheme = new Schema(
     {
         number: {
