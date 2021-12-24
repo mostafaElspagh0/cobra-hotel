f1bf8ab875a27819db26778de70989847260d4a6
MoOsman5 <mohamed1592000gmail.com>
2021-12-24 14:28:30 +0200

    creat room scheme

---
+++ backend/services/database/models/rooms.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const bcrypt = require('bcryptjs');
+const config = require('config');
+const roomScheme = new Schema(
+    {
+        number: {
+            type :Number,
+            require :true,
+        },
+        type :{
+            type :String,
+            require : true
+        },
+        full: {
+            type :Boolean,
+            require: true
+        },
+        who_in_room :{
+            type : String,
+            require : false
+        }
+
+    });
+const userModel = mongoose.model('room', roomScheme);
