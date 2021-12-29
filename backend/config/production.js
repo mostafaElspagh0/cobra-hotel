ffe373056d7cf55bb994096e2cbf4e3f69e927ef
mostafaElspagh0 <mostafaelspagh0@gmail.com>
2021-12-29 11:44:43 +0200

    fix css

--- backend/config/production.js
+++ backend/config/production.js
@@ -4,7 +4,7 @@ module.exports = {
     },
     'jwt_config':{ expiresIn: process.env.JWT_EXPIRE_IN, algorithm:  process.env.JWT_ALGORITHM },
     "jwtSecret": process.env.JWT_SECRET,
-    "SALT_WORK_FACTOR" : process.env.SALT_WORK_FACTOR,
+    "SALT_WORK_FACTOR" : process.env.SALT_WORK_FACTOR*1,
     "corsOrigin": process.env.FRONTEND_ORIGIN,
     "perPage":10,
     "gmail":{
